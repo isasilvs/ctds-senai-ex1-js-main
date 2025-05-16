44dae9ec681f4dbd931dd0c9f8600e685bbf5564
Isabelly Santos <isabellyfelixsantos12@gmail.com>
2025-05-16 19:31:07 -0300

    Add files via upload

---
+++ ex1.js
@@ -0,0 +1,24 @@
+/*
+Instruções:
+Crie um HTML básico e inclua o link externo para o arquivo script.js
+
+No JavaScript:
+Crie duas variáveis com números e exiba a soma delas no console
+
+Mostre no console.log() uma mensagem assim:
+console.log("A soma é:", soma);
+
+Use:
+let
+console.log()
+
+*/
+
+
+
+let num1= 45
+let num2= 35
+soma= num1 + num2
+console.log("A soma é:", soma);
+
+
